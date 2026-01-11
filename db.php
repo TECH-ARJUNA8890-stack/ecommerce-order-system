6a98656fada45d99b5b2d83441bfb4dc771266ec
TECH-ARJUNA8890-stack <arjunpandey8890k@gmail.com>
2026-01-11 11:04:17 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,16 @@
 <?php
-$conn = new mysqli(
-    getenv("dpg-d5hj4ip4tr6s739kst2g-a"),
-    getenv("ecommerce_user"),
-    getenv("E435MVkwEqwoSZHkGrBa1KEKpkOb7Wvu"),
-    getenv("ecommerce_db_2uvj")
-);
+$host = getenv("dpg-d5hj4ip4tr6s739kst2g-a");
+$user = getenv("ecommerce_user");
+$pass = getenv("E435MVkwEqwoSZHkGrBa1KEKpkOb7Wvu");
+$db   = getenv("DB_NAME");
+$port = getenv("5432") ?: 5432;
 
-if($conn->connect_error){
-    die("Database Connection Failed");
+$conn = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass");
+
+if (!$conn) {
+    die("Database connection failed!");
 }
 ?>
+
+
 
